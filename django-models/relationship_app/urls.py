623f7e4dcea79d6dfbba75c4f468be70b6c9905f
Mouhssin Aissam <koala3901@gmail.com>
2024-08-17 19:11:31 +0100

    s

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,10 @@
-# users/urls.py
 from django.urls import path
-from .views import Admin_view, librarian_view, member_view
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', Admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    # Other URL patterns for your app
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,29 +1,28 @@
-# users/views.py
-from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.views.generic.detail import DetailView 
+from .models import Library, Book  
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 
-def is_Admin(user):
-    return user.userprofile.role == 'Admin'
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-@user_passes_test(is_Admin)
-def Admin_view(request):
-    return render(request, 'admin_view.html')
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
