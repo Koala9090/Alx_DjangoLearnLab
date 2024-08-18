0d74041ee3aecae0e1989ce0101704e31b0d03d0
Mouhssin Aissam <koala3901@gmail.com>
2024-08-18 04:46:16 +0100

    s

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,27 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
 
-ROLE_CHOICES = (
-    ('Admin', 'Admin'),
-    ('Librarian', 'Librarian'),
-    ('Member', 'Member'),
-)
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can edit a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
     def __str__(self):
-        return f'{self.user.username} - {self.role}'
+        return self.title
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+
+
+# from django.db import models
+# from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+
+# class UserProfile(models.Model):
+#     ROLE_CHOICES = [
+#         ('Admin', 'Admin'),
+#         ('Librarian', 'Librarian'),
+#         ('Member', 'Member'),
+#     ]
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+#     def __str__(self):
+#         return self.user.username
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.userprofile.save()
 
 
 # from django.db import models
