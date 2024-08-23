3c4ca72419762a1c6a285db0a2b03a66eff47b22
Mouhssin Aissam <koala3901@gmail.com>
2024-08-23 18:57:55 +0100

    security

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,27 +1,30 @@
 from django.contrib import admin
-from .models import Book
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUserManager
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUserManager
-    list_display = ('email', 'date_of_birth', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+from .models import Book, CustomUser
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'date_of_birth', 'profile_photo')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo')}
         ),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields = ('username', 'email')
+    ordering = ('-date_joined',)
 
-admin.site.register(CustomUserManager, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
--- advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,16 +1,18 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None, **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, date_of_birth=date_of_birth, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -19,18 +21,51 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, date_of_birth, password, **extra_fields)
-class AbstractUser(AbstractUser):
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.username
+    
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view document"),
+            ("can_create", "Can create document"),
+            ("can_edit", "Can edit document"),
+            ("can_delete", "Can delete document"),
+        ]
+
+    def __str__(self):
+        return self.title
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        permissions = [
+            ("can_view_article", "Can view article"),
+            ("can_create_article", "Can create article"),
+            ("can_edit_article", "Can edit article"),
+            ("can_delete_article", "Can delete article"),
+        ]
 
     def __str__(self):
         return self.title
-
