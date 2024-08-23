8e5ca354f53113750253306154eb1be90bc56db8
Mouhssin Aissam <koala3901@gmail.com>
2024-08-23 16:23:30 +0100

    security

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUserManager
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUserManager
+    list_display = ('email', 'date_of_birth', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUserManager, CustomUserAdmin)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
