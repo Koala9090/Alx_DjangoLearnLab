58cf95db4b175133285c94aaf8fc193809a1ac82
Mouhssin Aissam <koala3901@gmail.com>
2024-08-24 15:40:49 +0100

    CSRF

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uh8oj-sezi*v!_uqf)o_)v3r5=5@l-((j*6ihvx)0$=)+flobb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,13 +3,11 @@ from django.contrib.auth.decorators import permission_required
 from .forms import BookForm
 from .models import Book
 
-# List Books View
 @permission_required('your_app.can_view_article', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
-# Create Book View
 @permission_required('your_app.can_create_article', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
@@ -21,7 +19,6 @@ def create_book(request):
         form = BookForm()
     return render(request, 'create_book.html', {'form': form})
 
-# Edit Book View
 @permission_required('your_app.can_edit_article', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
@@ -34,7 +31,6 @@ def edit_book(request, book_id):
         form = BookForm(instance=book)
     return render(request, 'edit_book.html', {'form': form})
 
-# Delete Book View
 @permission_required('your_app.can_delete_article', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
