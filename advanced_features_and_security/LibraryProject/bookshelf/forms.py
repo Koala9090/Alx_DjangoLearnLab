3d094ba67cde912f39f2cc80f36984a23a5fea75
Mouhssin Aissam <koala3901@gmail.com>
2024-08-24 15:47:09 +0100

    added ExampleForm

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100)
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
