d086f373a68d1ea0764e33d186fa249313ce06a4
Aissam <koala3901@gmail.com>
2024-09-18 17:39:11 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio', 'token')
         extra_kwargs = {
             'first_name': {'required': True},
             'last_name': {'required': True}
@@ -22,15 +24,16 @@ class UserSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             bio=validated_data.get('bio', '')
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        token, created = Token.objects.get_or_create(user=user)
+        user.token = token.key
         return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
